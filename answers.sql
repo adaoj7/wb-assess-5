e8264043f08751cbeb7d735683a6c6f0dc988969
Adam Johnson <adaojohnson902@gmail.com>
2023-09-01 12:31:26 -0600

    first part done

--- answers.sql
+++ answers.sql
@@ -1,7 +1,15 @@
-﻿-- Problem 1
-
+﻿SELECT * FROM cupcakes;
+SELECT * FROM customers;
+SELECT * FROM orders;
+-- Problem 1
+SELECT email FROM customers ORDER BY email;
 -- Problem 2
-
+SELECT * FROM orders WHERE customer_id = (SELECT id FROM customers WHERE fname = 'Elizabeth' AND lname = 'Crocker') ORDER BY customer_id;
 -- Problem 3
-
+SELECT SUM(num_cupcakes) FROM orders WHERE processed = false
 -- Problem 4
+SELECT cupcakes.name, SUM(orders.num_cupcakes) FROM cupcakes  LEFT JOIN orders ON  cupcakes.id = orders.cupcake_id GROUP BY cupcakes.name ORDER BY cupcakes.name;
+-- Problem 5
+SELECT customers.email, SUM(orders.num_cupcakes) AS total FROM customers LEFT JOIN orders ON customers.id = orders.customer_id GROUP BY customers.email ORDER BY customers.email;
+-- Problem 6
+SELECT fname, lname, email FROM customers JOIN orders ON customers.id = orders.customer_id WHERE cupcake_id = (SELECT id FROM cupcakes WHERE name = 'funfetti') AND orders.processed = true GROUP BY customers.fname, customers.lname, customers.email;
